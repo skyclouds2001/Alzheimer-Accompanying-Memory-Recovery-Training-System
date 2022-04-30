ab342b63fa4c0956a77f991cea5f38e5cb1eb97a
skyclouds2001 <skyclouds2001@163.com>
2022-04-30 13:12:14 +0800

    更新checkin页

--- pages/checkin/checkin.js
+++ pages/checkin/checkin.js
@@ -1,65 +1,27 @@
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    url: './../../images/psc.jpg',
+    clock: 0,
+    minDate: new Date().getTime(),
+    maxDate: new Date().getTime(),
+    defaultDate: [
+      new Date().getTime(),
+    ],
+    formatter: function (day) {
+      return day;
+    },
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onLoad: function () {
+    const date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = new Date(year, month + 1, 0).getDate();
+    this.setData({
+      minDate: new Date(year, month, 1).getTime(),
+      maxDate: new Date(year, month, day).getTime(),
+    });
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  },
 });
