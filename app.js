06fa8fd0ff20b04ffd954a28e899b8816c33424b
skyclouds2001 <skyclouds2001@163.com>
2022-05-05 22:39:40 +0800

    修改登录模块

--- app.js
+++ app.js
@@ -1,47 +1,34 @@
 'use strict';
 
 import { formatTime } from './utils/util.js';
-// import { request } from './lib/request.js';
+import { request } from './lib/request.js';
 
 App({
 
   onLaunch: async function () {
     try {
-      // // 登录
-      // const { code } = await wx.login({
-      //   timeout: 15000,
-      // });
-      // console.log({ code });
+      // 登录
+      const { code } = await wx.login({
+        timeout: 15000,
+      });
+      console.log({ code });
 
-      // // 获取密钥
-      // const { iv, encryptedData } = await wx.getUserInfo({
-      //   withCredentials: true,
-      //   lang: 'zh_CN',
-      // });
-      // console.log({ iv });
-      // console.log({ encryptedData });
+      // 向服务器请求换取 openid 与 token
+      const { data: res } = await request({
+        url: `/v1/user/login/${code}`,
+        method: 'GET',
+      });
+      console.log(res);
 
-      // // 向服务器请求换取 openid 与 token
-      // const { data: res } = await request({
-      //   url: '/v1/login/wx',
-      //   method: 'POST',
-      //   data: {
-      //     code,
-      //     iv,
-      //     encryptedData,
-      //   },
-      //   header: {
-      //     'content-type': 'application/json',
-      //   },
-      // });
-      // console.log(res);
-      // const { openid, token } = res;
-      // console.log({ openid, token });
+      // todo:
+      const { openid, token } = res.data;
+      console.log({ openid, token });
 
-      // this.globalData.openid = openid;
-      // this.globalData.token = token;
-      // wx.setStorageSync('openid', openid);
-      // wx.setStorageSync('token', token);
+      // 将 openid 与 token 存至 globalData 和 storage 内
+      this.globalData.openid = openid;
+      this.globalData.token = token;
+      wx.setStorageSync('openid', openid);
+      wx.setStorageSync('token', token);
     } catch (err) {
       console.log(err);
     }
