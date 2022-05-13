585a822efaac84dcdee97b41b45416ccfd54012a
skyclouds2001 <skyclouds2001@163.com>
2022-05-13 23:15:01 +0800

    修复tabbar显示异常bug

--- pages/index/index.js
+++ pages/index/index.js
@@ -13,4 +13,12 @@ Page({
     ],
   },
 
+  onShow: function () {
+    if (typeof this.getTabBar === 'function' && this.getTabBar()) {
+      this.getTabBar().setData({
+        select: 0,
+      });
+    }
+  },
+
 });
