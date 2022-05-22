e57f3d840cf9e302e2a4c02ed25e22223d281f6c
skyclouds2001 <skyclouds2001@163.com>
2022-05-22 15:55:09 +0800

    feat 时间线组件

--- components/timeline/timeline.js
+++ components/timeline/timeline.js
@@ -1,23 +1,47 @@
-// components/timeline/timeline.js
 Component({
-  /**
-   * 组件的属性列表
-   */
+
   properties: {
+    /**
+     * 线条及文字颜色颜色
+     */
+    color: {
+      type: String,
+      value: '#000',
+    },
 
+    /**
+     * 训练时间
+     */
+    time: {
+      type: String,
+      value: '',
+    },
+
+    /**
+     * 训练内容
+     */
+    text: {
+      type: String,
+      value: '',
+    },
+
+    /**
+     * 箭头方向
+     * * `true` left
+     * * `false` right
+     */
+    direction: {
+      type: Boolean,
+      value: true,
+    },
+
+    /**
+     * z-index优先级
+     */
+    zindex: {
+      type: Number,
+      value: 0,
+    },
   },
 
-  /**
-   * 组件的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 组件的方法列表
-   */
-  methods: {
-
-  },
 });
