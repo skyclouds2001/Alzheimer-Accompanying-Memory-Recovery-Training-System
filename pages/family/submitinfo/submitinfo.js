99aa39f91031eec6dfae9374c7006dada16fb6a4
s5das <96362979+s5das@users.noreply.github.com>
2022-05-27 16:29:25 +0800

    接口初步完成

--- pages/family/submitinfo/submitinfo.js
+++ pages/family/submitinfo/submitinfo.js
@@ -1,37 +1,37 @@
 // pages/submitinfo/submitinfo.js
 import Toast from '@vant/weapp/toast/toast';
+import { request } from '../../../lib/request.js';
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    avatarUrl:'',
-    nickName:'',
+    avatarUrl: '',
+    nickName: '',
     // 性别
-    array1:['男','女'],
-    index1:0,
+    array1: ['点击选择', '男', '女'],
+    index1: 0,
     // 国家和地区
-    array2:['中国','其他'],
-    index2:0,
+    array2: ['点击选择', '中国', '其他'],
+    index2: 0,
     // 文化程度
-    index3:0,
-    array3:['低','中',"高"],
+    index3: 0,
+    array3: ['点击选择', '低', '中', '高'],
     // 出生地
-    place1:['无'],
-    customitem:"无",
+    place1: ['无'],
+    customitem: '无',
     // 居住地
-    place2:["无"],
+    place2: ['无'],
     // 照片
-    fileList: [{url: 'https://img.yzcdn.cn/vant/leaf.jpg'}]
-
+    fileList: [{ url: 'https://img.yzcdn.cn/vant/leaf.jpg' }],
 
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  onLoad (options) {
     const userInfo = wx.getStorageSync('userInfo') || {};
     this.setData({
       avatarUrl: userInfo.avatarUrl,
@@ -39,50 +39,115 @@ Page({
     });
   },
 
-//  选项处理
-  bindPickerChange1(event){
-    const {value} = event.detail;
+  //  选项处理
+  bindPickerChange1 (event) {
+    const { value } = event.detail;
     this.setData({
-      index1:Number(value)
-    })
+      index1: Number(value),
+    });
   },
 
-  bindPickerChange2(event){
-    const {value} = event.detail;
+  bindPickerChange2 (event) {
+    const { value } = event.detail;
     this.setData({
-      index2:Number(value)
-    })
+      index2: Number(value),
+    });
   },
-  bindPickerChange3(event){
-    const {value} = event.detail;
+  bindPickerChange3 (event) {
+    const { value } = event.detail;
     this.setData({
-      index3:value
-    })
+      index3: value,
+    });
   },
 
-  bindPickerChange4(event){
-    const {value} = event.detail;
+  bindPickerChange4 (event) {
+    const { value } = event.detail;
     this.setData({
-      place1:value
-    })
-      
+      place1: value,
+    });
   },
-  bindPickerChange5(event){
-    const {value} = event.detail;
+  bindPickerChange5 (event) {
+    const { value } = event.detail;
     this.setData({
-      place2:value
-    })
-      
+      place2: value,
+    });
   },
-  formSubmit(event){
-
-    let {value}= event.detail
-    value.img = this.data.fileList
+  formSubmit (event) {
+    const { value } = event.detail;
+    if (this.data.fileList.length) { value.photos = this.data.fileList[0].url; } else { value.img = ''; }
     console.log(value);
-    Toast.success("上传成功")
+    if (this.checkInfo(value)) {
+      this.postInfo(value);
+    } else {
+      Toast.fail('请输入完整信息');
+    }
   },
-  // 文件上传
-  uoload(event){
-     console.log(event);
-  }
-})
+  /** 提交信息 */
+  postInfo (value) {
+    const value2 = {};
+
+    // 要传的六个字段-------------------
+    value2.age = value.age;
+    value2.address = value.address;
+    value2.province = value.province;
+    value2.eduBgcground = value.eduBgcground;
+    value2.photos = value.photos;
+    value2.pname = value.pname;
+    // --------------------------------
+
+    const token = wx.getStorageSync('token');
+    const p = request({ url: '/v1/patient', method: 'PUT', header: { authorization: token }, data: value2 });
+    p.then(() => { Toast.success('putsuccess'); }, () => { Toast.fail('putfail'); });
+  },
+  /**
+   *检测信息
+   * @param {} obj
+   */
+  checkInfo (obj) {
+    for (const key in obj) {
+      if (!obj[key]) {
+        return false;
+      }
+    }
+    return true;
+  },
+  /**
+   * 上传文件返回在线地址
+   * @param {e} event
+   */
+  uoload (event) {
+    const { url } = event.detail.file[0];
+    console.log(url);
+    const that = this;
+    const p = new Promise((resolve, reject) => {
+      wx.uploadFile({
+        url: 'https://example.weixin.qq.com/upload', // 仅为示例，非真实的接口地址
+        filePath: url,
+        name: 'file',
+        formData: {
+          user: 'test',
+        },
+        success (res) {
+          resolve(res.data);
+        },
+        fail (err) {
+          reject(err);
+        },
+      });
+    },
+    );
+
+    p.then((value) => { that.setData({ fileList: [{ url: value }] }); }, (err) => { console.log(err); });
+  },
+
+  /**
+   * del event
+   * @param {e} event
+   */
+  del (event) {
+    const { index } = event.detail;
+    const fileList = this.data.fileList;
+    fileList.splice(index, 1);
+    this.setData({ fileList });
+  },
+});
