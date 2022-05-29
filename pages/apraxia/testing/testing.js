2a5be1eaa57c5939a039b417f29707c6fba9f764
Ice-cream17935 <73979817+Ice-cream17935@users.noreply.github.com>
2022-05-29 20:04:15 +0800

    手势

--- pages/apraxia/testing/testing.js
+++ pages/apraxia/testing/testing.js
@@ -9,10 +9,9 @@ Page({
   data: {
     imageUrl1: '../../../images/example.jpg',
     imageUrl2: '../../../images/example.jpg',
-    btn_enable: true,
+    btn_enable: '0',
     result: '',
   },
-
   onReady: function (res) {
     // get access_token from BaiDu API
     wx.request({
@@ -30,80 +29,83 @@ Page({
       },
     });
   },
-
+  chooseImg: function () {
+    return new Promise((resolve, reject) => {
+      const that = this;
+      wx.chooseImage({
+        count: 1,
+        sizeType: ['original', 'compressed'],
+        sourceType: ['album', 'camera'],
+        success (res) {
+          const tempFilePaths = res.tempFilePaths;
+          console.log(tempFilePaths);
+          apiUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/gesture';
+          that.setData({
+            imageUrl1: tempFilePaths,
+            btn_enable: '1',
+          });
+          console.log('My API URL is : ' + apiUrl);
+          console.log('Image Path is : ' + tempFilePaths);
+          // console.log(res)
+          wx.getFileSystemManager().readFile({
+            filePath: res.tempFilePaths[0],
+            encoding: 'base64',
+            // complete: res=> {
+            //   console.log('complete')
+            //   console.log(res)
+            // },
+            success: res => {
+              base64 = res.data;
+              resolve(base64);
+            // console.log('data:image/png;base64,' + base64)
+            },
+          });
+        },
+      });
+    });
+  },
   get_image: function (res) {
     const that = this;
-    that.setData({
-      load_title: '正在上传',
-      load_message: '正在上传图片,请稍后',
-    });
-
-    wx.chooseImage({
-      count: 1,
-      sizeType: ['original', 'compressed'],
-      sourceType: ['album', 'camera'],
-      success (res) {
-        const tempFilePaths = res.tempFilePaths;
-        apiUrl = 'https://aip.baidubce.com/rest/2.0/image-classify/v1/gesture';
-        that.setData({
-          imageUrl1: tempFilePaths,
-          btn_enable: true,
-        });
-        console.log('My API URL is : ' + apiUrl);
-        console.log('Image Path is : ' + tempFilePaths);
-        // console.log(res)
-        wx.getFileSystemManager().readFile({
-          filePath: res.tempFilePaths[0],
-          encoding: 'base64',
-          // complete: res=> {
-          //   console.log('complete')
-          //   console.log(res)
-          // },
-          success: res => {
-            base64 = res.data;
-            // console.log('data:image/png;base64,' + base64)
-          },
-        });
-      },
-    });
-
-    wx.request({
-      url: apiUrl + '?access_token=' + token,
-      method: 'POST',
-      header: {
-        'content-type': 'application/x-www-form-urlencoded',
-      },
-      data: {
-        image: encodeURI(base64),
-      },
-      success: res => {
-        console.log('recognition_image Success');
-        if (res.data.result == null) {
-          console.log(res.data.error_msg);
-          console.log(base64);
-        } else {
-          console.log(res);
-          that.setData({
-            result: res.data.result[0].classname,
+    this.chooseImg().then(res => {
+      wx.request({
+        url: apiUrl + '?access_token=' + token,
+        method: 'POST',
+        header: {
+          'content-type': 'application/x-www-form-urlencoded',
+        },
+        data: {
+          image: encodeURI(res),
+        },
+        success: res => {
+          console.log('recognition_image Success');
+          if (res.data.result == null) {
+            console.log(res.data.error_msg);
+            console.log(base64);
+          } else {
+            console.log(res);
+            that.setData({
+              result: res.data.result[0].classname,
+              imageUrl2: 'https://mgl-image.oss-cn-beijing.aliyuncs.com/gesture/22.jpeg',
+            });
+          }
+          ;
+          wx.request({
+            url: 'http://127.0.0.1/v1/gesture',
+            data: { recognizedName: that.result },
+            method: 'POST',
+            header: {
+              authorization: ' ',
+            },
+            success: (res) => {
+              that.setData({
+                imageUrl2: res.data.image,
+              });
+            },
+            fail: (res) => {},
+            complete: (res) => {},
           });
-        }
-        ;
-      },
-    });
-    wx.request({
-      url: 'http://127.0.0.1/v1/gesture',
-      data: { recognizedName: that.data.result },
-      method: 'POST',
-      header: {
-        authorization: ' ',
-      },
-      success: (res) => {
-        that.setData({
-          imageUrl2: res.data.image,
-        });
-      },
-      fail: (res) => {},
-      complete: (res) => {},
+        },
+      });
     });
   },
 
