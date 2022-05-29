196b63c2b10bb3af5b470b461ee6798e2865ac73
skyclouds2001 <skyclouds2001@163.com>
2022-05-29 17:17:47 +0800

    perf
    remove unused global logs and imports

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-import { formatTime } from './utils/util.js';
 import { request } from './lib/request.js';
 
 App({
@@ -9,24 +8,20 @@ App({
       const { code } = await wx.login({
         timeout: 15000,
       });
-      console.log({ code });
 
       // 向服务器请求换取 openid 与 token
       const { data: res } = await request({
         url: `/v1/user/login/${code}`,
         method: 'GET',
       });
-      console.log(res);
 
-      // todo:
       const { openid, token } = res.data;
-      console.log({ openid, token });
 
       // 将 openid 与 token 存至 storage 内
       wx.setStorageSync('openid', openid);
       wx.setStorageSync('token', token);
     } catch (err) {
-      console.log(err);
+      console.error(err);
     }
   },
 
@@ -40,25 +35,24 @@ App({
     });
     console.log({ res });
   },
-  onShow(){ 
-    if(this.globalData.firstIn){
-        this.globalData.firstIn = 0; 
-    } else{ 
-        this.globalData.onShow = 1; 
-    } 
-  }, 
-  onHide(){ 
-      this.globalData.onHide = 1; 
+
+  onShow () {
+    if (this.globalData.firstIn) {
+      this.globalData.firstIn = 0;
+    } else {
+      this.globalData.onShow = 1;
+    }
+  },
+  onHide () {
+    this.globalData.onHide = 1;
   },
 
   globalData: {
-    openid: '',
-    token: '',
-    mysongs:[],
-    firstIn:1,
-    onShow: 0, 
-    onHide: 0
-  },
-  
-});
+    mysongs: [],
 
+    firstIn: 1,
+    onShow: 0,
+    onHide: 0,
+  },
+
+});
