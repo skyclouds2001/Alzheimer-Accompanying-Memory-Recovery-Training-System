971b621da81f20ca6370da2cd67c5d0be2c071ba
skyclouds2001 <skyclouds2001@163.com>
2022-06-29 21:53:27 +0800

    ls

--- pages/apraxia/index/index.js
+++ pages/apraxia/index/index.js
@@ -1,3 +1,7 @@
+import { request } from '../../../lib/request';
+
+const cookie = 'MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/openapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/clientlog;;MUSIC_U=e84478a1b700c4d4f5f33898aa0a24fbcf34cb06c52baf0043bc2c5837c792b6519e07624a9f0053901f8918f05ad12e1901903b05cce24461dafccfd406cc3674816114a4195f687a561ba977ae766d; Max-Age=1296000; Expires=Mon, 23 May 2022 05:06:50 GMT; Path=/;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/openapi/clientlog;;__remember_me=true; Max-Age=1296000; Expires=Mon, 23 May 2022 05:06:50 GMT; Path=/;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/clientlog;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/clientlog;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/feedback;;__csrf=0a4170624e744ed399f4cadd3fa3939b; Max-Age=1296010; Expires=Mon, 23 May 2022 05:07:00 GMT; Path=/;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/clientlog;;MUSIC_SNS=; Max-Age=0; Expires=Sun, 08 May 2022 05:06:50 GMT; Path=/;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/clientlog;';
+
 /**
  * @typedef timestamp
  * @type {number}
@@ -18,73 +22,64 @@
  * @property {number} type
  */
 
+const app = getApp();
+
 Page({
 
   data: {
     /**
-     * 歌曲列表
+     * 歌曲列表1
      * @type {Song[]}
      */
-    array: [],
+    array1: [],
+
+    /**
+     * 歌曲列表2
+     * @type {Song[]}
+     */
     array2: [],
   },
 
-  onLoad: function () {
-    const that = this;
-    const app = getApp();
-    console.log();
-    wx.request({
-      url: 'https://api.xaneon.com/cloudsearch?',
-      method: 'post',
+  onLoad: async function () {
+    const { data: res1 } = await request({
+      url: 'https://api.xaneon.com/cloudsearch',
+      method: 'POST',
       data: {
-        cookie: app.globalData.cookie,
+        cookie: cookie,
         keywords: '阿尔法脑波音乐',
         type: 1000,
         timestamp: 1503019930000,
       },
       header: {
-        'content-type': 'application/json', // 默认值
-
-      },
-      success (res) {
-        console.log(res);
-        that.setData({
-          // 默认选取前9个歌单
-          array: res.data.result.playlists.slice(0, 2),
-        });
+        'Content-Type': 'application/json',
       },
     });
-    wx.request({
-      url: 'https://api.xaneon.com/cloudsearch?',
-      method: 'post',
+    const { data: res2 } = await request({
+      url: 'https://api.xaneon.com/cloudsearch',
+      method: 'POST',
       data: {
-        cookie: app.globalData.cookie,
+        cookie: cookie,
         keywords: '脑部治疗',
         type: 1000,
         timestamp: 1503019930011,
       },
       header: {
-        'content-type': 'application/json', // 默认值
+        'Content-Type': 'application/json', // 默认值
       },
-      success (res1) {
-        console.log(res1);
-        that.setData({
-          // 默认选取前9个歌单
-          array2: res1.data.result.playlists.slice(0, 2),
-        });
-      },
+    });
+    this.setData({
+      array1: res1.result.playlists.slice(0, 2),
+      array2: res2.result.playlists.slice(0, 2),
     });
   },
 
   /**
    * 跳转至歌曲详情页
-   * @function
-   * @param {Event} e
+   * @param {TouchEvent} e
    * @returns {void}
    */
   handleItem (e) {
     const { id } = e.currentTarget.dataset;
-    const app = getApp();
     app.globalData.id = id;
     wx.navigateTo({
       url: `../list/list?listid=${id}`,
@@ -93,10 +88,9 @@ Page({
 
   /**
    * 跳转至我的喜欢歌曲页
-   * @function
    * @returns {void}
    */
-  handleMySongs: function () {
+  handleMySongs () {
     wx.navigateTo({
       url: '../mysong/mysong',
     });
--- pages/apraxia/list/list.js
+++ pages/apraxia/list/list.js
@@ -1,3 +1,9 @@
+import { request } from '../../../lib/request';
+
+const cookie = 'MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/openapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/clientlog;;MUSIC_U=e84478a1b700c4d4f5f33898aa0a24fbcf34cb06c52baf0043bc2c5837c792b6519e07624a9f0053901f8918f05ad12e1901903b05cce24461dafccfd406cc3674816114a4195f687a561ba977ae766d; Max-Age=1296000; Expires=Mon, 23 May 2022 05:06:50 GMT; Path=/;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/openapi/clientlog;;__remember_me=true; Max-Age=1296000; Expires=Mon, 23 May 2022 05:06:50 GMT; Path=/;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/clientlog;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/clientlog;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/feedback;;__csrf=0a4170624e744ed399f4cadd3fa3939b; Max-Age=1296010; Expires=Mon, 23 May 2022 05:07:00 GMT; Path=/;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/clientlog;;MUSIC_SNS=; Max-Age=0; Expires=Sun, 08 May 2022 05:06:50 GMT; Path=/;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/feedback;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/neapi/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/wapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/api/clientlog;;MUSIC_R_T=1638277100823; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/eapi/feedback;;MUSIC_A_T=1638277100672; Max-Age=2147483647; Expires=Fri, 26 May 2090 08:20:57 GMT; Path=/weapi/clientlog;';
+
+const app = getApp();
+
 Page({
 
   data: {
@@ -21,38 +27,33 @@ Page({
     coverImg: '',
   },
 
-  onShow: function () {
-    const that = this;
-    const app = getApp();
-    const ts = Date.parse(new Date());
-    wx.request({
-      url: `https://api.xaneon.com/playlist/detail?timestamp=${ts}`, // 仅为示例，并非真实的接口地址
+  onLoad: async function () {
+    const { data: res } = await request({
+      url: 'https://api.xaneon.com/playlist/detail',
       method: 'POST',
       data: {
+        timestamp: Date.parse(new Date()),
         id: app.globalData.id,
-        cookie: app.globalData.cookie,
+        cookie: cookie,
       },
       header: {
-        'content-type': 'application/json', // 默认值
+        'Content-Type': 'application/json',
       },
-      success (res) {
-        that.setData({
-          song_array: res.data.playlist.tracks,
-          name: res.data.playlist.name,
-          coverImg: res.data.playlist.coverImgUrl,
-        });
-      },
+    });
+    this.setData({
+      song_array: res.playlist.tracks,
+      name: res.playlist.name,
+      coverImg: res.playlist.coverImgUrl,
     });
   },
 
   /**
    * 点击详细音乐跳转
    * @function
-   * @param {Event} e 点击事件对象
+   * @param {TouchEvent} e 点击事件对象
    * @returns {void}
    */
   handleSong: function (e) {
-    const app = getApp();
     app.globalData.song_image = e.currentTarget.dataset.picurl;
     const id = e.currentTarget.dataset.id;
     const name = e.currentTarget.dataset.song.name;
