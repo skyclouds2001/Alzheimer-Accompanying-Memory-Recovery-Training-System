b49825841cc04e74e55be327acdc7a138296e0dc
skyclouds2001 <skyclouds2001@163.com>
2022-07-17 16:56:51 +0800

    feat exercise.js
    抽离锻炼模块接口

--- api/exercise.js
+++ api/exercise.js
@@ -1,5 +1,10 @@
 import { request } from './../lib/request';
 
+/**
+ * 上传训练记录
+ * @param {string} token
+ * @param
+ */
 export const addExerciseRecord = async (token, {
   time: exTime,
   type: exType,
@@ -17,6 +22,34 @@ export const addExerciseRecord = async (token, {
         exTime,
         exType,
         score,
+      },
+    });
+    if (res.status === 10000) {
+      return Promise.resolve(res.data);
+    } else {
+      return Promise.reject(new Error(res.message));
+    }
+  } catch (err) {
+    return Promise.reject(err);
+  }
+};
+
+/**
+ * 上传训练记录
+ * @param {string} token
+ */
+export const getExerciseRecord = async (token, num = 1, size = 10) => {
+  try {
+    const { data: res } = await request({
+      url: '/v1/exercise/get',
+      method: 'GET',
+      header: {
+        authorization: token,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      data: {
+        PageNum: num,
+        PageSize: size,
       },
     });
     if (res.status === 10000) {
