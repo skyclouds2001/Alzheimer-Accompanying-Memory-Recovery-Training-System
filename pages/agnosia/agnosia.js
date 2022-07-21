bec206053231f16c3bb6c0ff04b5bf670c649b21
skyclouds2001 <skyclouds2001@163.com>
2022-07-22 02:22:17 +0800

    refactor agnosia
    抽离部分请求API
    优化代码逻辑
    移除冗余的代码

--- pages/agnosia/agnosia.js
+++ pages/agnosia/agnosia.js
@@ -2,19 +2,23 @@ import Toast from '@vant/weapp/toast/toast';
 import Dialog from '@vant/weapp/dialog/dialog';
 
 import { getQuestion } from './../../api/question';
-
-import { request } from '../../lib/request';
+import { getVoice } from './../../api/voice';
+import { addExerciseRecord } from './../../api/exercise';
 
 const token = wx.getStorageSync('token');
 
 // 答完题播放音频
 const audioRefEnding = wx.createInnerAudioContext();
+audioRefEnding.src = 'http://aimg8.dlszyht.net.cn/ueditor/file/823/1644869/1553422829215244.mp3';
 // 点击按钮音频
 const audioRefRadio = wx.createInnerAudioContext();
+audioRefRadio.src = 'http://gaofeifei.3vfree.cn/anniu/click.mp3';
 // 错误音效
 const audioRefFalse = wx.createInnerAudioContext();
+audioRefFalse.src = 'http://gaofeifei.3vfree.cn/anniu/fault.mp3';
 // 成功音效
 const audioRefTrue = wx.createInnerAudioContext();
+audioRefTrue.src = 'http://gaofeifei.3vfree.cn/anniu/victory.mp3';
 
 Page({
 
@@ -22,6 +26,7 @@ Page({
     /** 标记当前是否正在加载资源 */
     isloading: true,
 
+    /** 控制跳转按钮是否启用 */
     isdisable: true,
 
     /** 答题阶段 | 结束阶段 */
@@ -30,33 +35,25 @@ Page({
     // 弹出框显示
     show: false,
     judge: '正确',
-    // 按钮文字
-    submit_text: '下一题',
-    // 题目组索引
-    index: 0,
-    // 问题对象
-    question_obj: [],
+
+    // 当前题目索引
+    index: -1,
+
+    // 问题数据
+    questions: [],
 
     // 成绩
     score: 0,
+
+    // 选择选项
     radio: -1,
-    // 结尾音频地址
-    audio_src:
-      'http://aimg8.dlszyht.net.cn/ueditor/file/823/1644869/1553422829215244.mp3',
   },
 
   onLoad: function () {
-    // 初始化音频文件
-    audioRefEnding.obeyMuteSwitch = false; // 是否遵循系统静音原则
-    audioRefEnding.src = this.data.audio_src; // 音频地址
-    audioRefRadio.src = 'http://gaofeifei.3vfree.cn/anniu/click.mp3';
-    audioRefFalse.src = 'http://gaofeifei.3vfree.cn/anniu/fault.mp3';
-    audioRefTrue.src = 'http://gaofeifei.3vfree.cn/anniu/victory.mp3';
-
     // 显示提示确认框
     Dialog.confirm({
       title: '提示',
-      message: '现在开始训练',
+      message: '确认开始训练吗',
       confirmButtonText: '开始训练',
       cancelButtonText: '取消训练',
     }).then(async () => {
@@ -65,18 +62,21 @@ Page({
       // 获取训练题库
       try {
         const res = await getQuestion(token, 0, 0, 10);
-        res.forEach(v => (v.checks = JSON.parse(v.checks)));
-        setTimeout(() => {
-          this.setData({
-            question_obj: res,
-            isloading: false,
-          });
-        }, 1500);
+        res.forEach(v => (v.checks = JSON.parse(v.checks))); // 转义字符串类型选项为数组类型
+        this.setData({
+          questions: res,
+          index: 0,
+        });
       } catch (err) {
         console.error(err);
         Toast.fail('网络异常');
       }
-      // this.getVoice();
+
+      // 获取结束答题语音 todo
+      try {
+        const res = await getVoice(token);
+        audioRefEnding.src = res.data;
+      } catch (err) {}
     }).catch(() => {
       Toast('即将退出训练');
       setTimeout(() => {
@@ -84,70 +84,16 @@ Page({
           delta: 1,
         });
       }, 1500);
-    });
-  },
-
-  // 点击submit按钮事件
-  change () {
-    audioRefRadio.play();
-    this.setData({
-      isdisable: true,
-    });
-
-    // 点击下一题
-    if (this.data.index < this.data.question_obj.length - 1) {
-      if (this.data.radio === this.data.question_obj[this.data.index].answer) {
+    }).finally(() => {
+      wx.nextTick(() => {
         this.setData({
-          judge: '正确',
-          score: this.data.score + 1,
+          isloading: false,
         });
-        audioRefTrue.play();
-      } else {
-        this.setData({
-          judge: '错误',
-        });
-        audioRefFalse.play();
-      }
-      this.setData({
-        show: true,
       });
-      setTimeout(this.disappear, 2000);
-    } else {
-      // 点击提交，算成绩放音乐
-      if (this.data.radio === this.data.question_obj[this.data.index].answer) {
-        audioRefTrue.play();
-        this.setData({
-          score: this.data.score + 1,
-        });
-      } else {
-        audioRefFalse.play();
-      }
-      audioRefEnding.play();
-      this.setData({
-        condition: true,
-      });
-      this.sendscore();
-    }
-  },
-
-  disappear () {
-    this.setData({
-      show: false,
-      index: this.data.index + 1,
-      radio: '-1',
-    });
-    if (this.data.index === this.data.question_obj.length - 1) {
-      this.setData({
-        submit_text: '提交',
-      });
-    }
-  },
-
-  onChange (e) {
-    this.setData({
-      radio: e.detail,
     });
   },
+
+  /** 选项选择 */
   onClick (e) {
     audioRefRadio.play();
     const { name } = e.currentTarget.dataset;
@@ -157,26 +103,72 @@ Page({
     });
   },
 
-  // 训练结束发送成绩
-  sendscore () {
-    request({
-      url: '/v1/exercise/add',
-      data: { exTime: 0, exType: 2, score: this.data.score },
-      method: 'POST',
-    }).then(
-      () => {
-        console.log('push successfully');
-      },
-      () => {
-        console.log('push fail');
-      },
-    );
-  },
+  /**
+   * 点击submit按钮事件
+   */
+  async handleSubmitAnswer () {
+    const { index, questions, radio, score } = this.data;
+    this.setData({
+      isdisable: true,
+    });
 
-  // getVoice () {
-  //   const taht = this;
-  //   const p3 = request({ url: '/v1/voice', header: { 'Content-Type': 'application/json', authorization: token } });
-  //   p3.then((res) => { console.log(this); this.setData({ audio_src: res.data }); }, (err) => { console.error(err); });
-  // },
+    if (index < questions.length - 1) {
+      // 不是最后一题 点击下一题
+
+      if (radio === questions[index].answer) {
+        this.setData({
+          judge: '正确',
+          score: score + 1,
+          show: true,
+        });
+        audioRefTrue.play();
+      } else {
+        this.setData({
+          judge: '错误',
+          show: true,
+        });
+        audioRefFalse.play();
+      }
+
+      setTimeout(() => {
+        this.setData({
+          show: false,
+          index: index + 1,
+          radio: -1,
+        });
+      }, 2000);
+    } else {
+      // 是最后一题 点击提交，算成绩放音乐
+      if (radio === questions[index].answer) {
+        audioRefTrue.play();
+        this.setData({
+          score: score + 1,
+        });
+      } else {
+        audioRefFalse.play();
+      }
+
+      // 播放结束答题声音
+      audioRefEnding.play();
+
+      // 更新状态 答题结束
+      this.setData({
+        condition: true,
+      });
+
+      // 提交训练记录
+      try {
+        const { score } = this.data;
+        await addExerciseRecord(token, {
+          time: 0,
+          type: 1,
+          score,
+        });
+      } catch (err) {
+        console.error(err);
+        Toast.fail('网络异常');
+      }
+    }
+  },
 
 });
