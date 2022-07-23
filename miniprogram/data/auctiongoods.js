29ffd999578460713800ca26eca1644f8f20fcf6
skyclouds2001 <skyclouds2001@163.com>
2022-07-23 13:23:51 +0800

    refactor goods
    抽离商品数据

---
+++ miniprogram/data/auctiongoods.js
@@ -0,0 +1,30 @@
+export const goods = [
+  {
+    id: 0,
+    title: '新鲜土豆',
+    url: 'https://img10.360buyimg.com/n1/jfs/t1/197514/10/10565/78064/615426bdE194204e5/0cacfd0720850630.jpg',
+    price: 78,
+    isLiked: false,
+  },
+  {
+    id: 1,
+    title: '新鲜红辣椒',
+    url: 'https://img13.360buyimg.com/n1/jfs/t1/86315/7/27859/250515/624e7485E18525e04/c443eefa1decb5c9.jpg',
+    price: 96,
+    isLiked: false,
+  },
+  {
+    id: 2,
+    title: '胡萝卜',
+    url: 'https://img12.360buyimg.com/n1/jfs/t1/126556/32/14205/51873/5f796521Efc0677c7/e29fbbfe75afc0fb.jpg',
+    price: 34,
+    isLiked: false,
+  },
+  {
+    id: 3,
+    title: '优质洋葱',
+    url: 'https://img11.360buyimg.com/n1/jfs/t1/175321/19/26645/92332/6204b308E70d15059/fb68e7c3fc3bbb01.jpg',
+    price: 57,
+    isLiked: false,
+  },
+];
