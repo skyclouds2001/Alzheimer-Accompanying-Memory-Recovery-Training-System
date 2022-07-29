e9c7b840281d165c46a85c53a094c6f02afa5ab3
skyclouds2001 <skyclouds2001@163.com>
2022-07-29 20:46:36 +0800

    refactor recalltime

--- pages/family/Recalltime/Recalltime.js
+++ pages/family/Recalltime/Recalltime.js
@@ -1,9 +1,5 @@
-// pages/family/Recalltime/Recalltime.js
 Page({
 
-  /**
-   * 页面的初始数据
-   */
   data: {
     /** 相册数量 */
     albumquantity: 0,
@@ -12,77 +8,5 @@ Page({
     /** 视频数量 */
     videoquantity: 0,
   },
-  // falshtabbar
-  photo () {
-    wx.redirectTo({
-      url: '/pages/family/Recalltime/photo/photo',
-    });
-  },
-  vedio () {
-    wx.redirectTo({
-      url: '/pages/family/Recalltime/vedio/vedio',
-    });
-  },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    // tabbar页面相关
-    // if (typeof this.getTabBar === 'function' && this.getTabBar()) {
-    //   this.getTabBar().setData({
-    //     select: 0,
-    //     kind: 2,
-    //   });
-    // }
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage () {
-
-  },
 });
